cc462847f514cb80bb37f02fdb3e4abde050fbc9
Ravenium22 <efeberkakgul@gmail.com>
2024-12-30 06:58:24 +0300

    Move HTML and src to public, update asset paths

--- public/src/game.js
+++ public/src/game.js
@@ -94,35 +94,35 @@ class RamenRunner {
   loadAssets() {
     // Background
     this.backgroundImage = new Image();
-    this.backgroundImage.src = 'sprites/background.png';
+    this.backgroundImage.src = '/sprites/background.png';
 
     // Character
     this.playerSprite = new Image();
     const selectedCharacter =
       localStorage.getItem('selectedCharacter') || 'runner.png';
     console.log('[loadAssets] selectedCharacter =', selectedCharacter);
-    this.playerSprite.src = `sprites/${selectedCharacter}`;
+    this.playerSprite.src = `/sprites/${selectedCharacter}`;
 
     // Obstacles
     this.obstacleSprites = ['obstacle1.png', 'obstacle2.png', 'obstacle3.png'].map(
       (src) => {
         const img = new Image();
-        img.src = `sprites/${src}`;
+        img.src = `/sprites/${src}`;
         return img;
       }
     );
 
     // Power-up
     this.powerUpSprite = new Image();
-    this.powerUpSprite.src = 'sprites/powerup.png';
+    this.powerUpSprite.src = '/sprites/powerup.png';
 
     // Heart icon
     this.heartSprite = new Image();
-    this.heartSprite.src = 'sprites/heart.png';
+    this.heartSprite.src = '/sprites/heart.png';
 
     // Audio
-    this.jumpSound = new Audio('audio/jump.mp3');
-    this.bgMusic = new Audio('audio/music.mp3');
+    this.jumpSound = new Audio('/audio/jump.mp3');
+    this.bgMusic = new Audio('/audio/music.mp3');
     this.bgMusic.loop = true;
   }
 
